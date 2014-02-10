27af744b4222a5ecb19b9d977b0647ffb9c564c0
Yanxing Wang <demiwangya@gmail.com>
2014-02-10 15:27:17 -0800

    integration2

--- POST/src/Product_Catalog.java
+++ POST/src/Product_Catalog.java
@@ -1,3 +1,7 @@
+/*
+ * SFSU CSC 668/868 Lab
+ * Post 1
+ */
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -5,11 +9,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author abitchue
